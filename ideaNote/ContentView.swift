07f8e242ab9fa3c5f494aad215832dd9d89e87a1
alex.kao <wenhao.kao@rccchina.com>
2022-10-12 17:02:55 +0800

    Initial Commit

---
+++ ideaNote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ideaNote
+//
+//  Created by kwh on 2022/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ideaNote/ideaNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ideaNoteApp.swift
+//  ideaNote
+//
+//  Created by kwh on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct ideaNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
